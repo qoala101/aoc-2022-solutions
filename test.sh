6aa2c3548a3270003cb4e852aa177c994135a264
Volodymyr Hromakov <4y5t6r@gmail.com>
2022-12-04 19:38:21 +0200

    Added test script.

---
+++ test.sh
@@ -0,0 +1,14 @@
+diff <(
+  ./bin/day1 ./inputs/day1
+  ./bin/day2_part1 ./inputs/day2
+  ./bin/day2_part2 ./inputs/day2
+  ./bin/day3_part1 ./inputs/day3
+  ./bin/day3_part2 ./inputs/day3
+) <(
+  echo 72070
+  echo 211805
+  echo 11906
+  echo 11186
+  echo 8039
+  echo 2510
+)
